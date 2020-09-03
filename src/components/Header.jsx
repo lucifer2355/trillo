4f91d2c91abcf33a886eff4389aa1707a97009af
lucifer2355 <dgajjar999@gmail.com>
2020-09-04 00:18:08 +0530

    add images in public folder

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Header = () => {
-  return <header className='header'>Header Part</header>;
+  return (
+    <header className='header'>
+      <img src='./images/logo.png' alt='trillo logo' className='logo' />
+
+      <form action='#' className='search'>
+        <input
+          type='text'
+          className='search__input'
+          placeholder='Search hotels'
+        />
+        <button></button>
+      </form>
+    </header>
+  );
 };
 
 export default Header;
