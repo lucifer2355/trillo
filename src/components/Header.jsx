9095856a5245d84d0c962480cbaa177cfded69c6
lucifer2355 <dgajjar999@gmail.com>
2020-09-04 00:40:29 +0530

    add header html

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,6 +17,29 @@ const Header = () => {
           </svg>
         </button>
       </form>
+
+      <nav className='user-nav'>
+        <div className='user-nav__icon-box'>
+          <svg className='user-nav__icon'>
+            <use xlinkHref='/images/sprite.svg#icon-bookmark'></use>
+          </svg>
+          <span className='user-nav__notification'>7</span>
+        </div>
+        <div className='user-nav__icon-box'>
+          <svg className='user-nav__icon'>
+            <use xlinkHref='/images/sprite.svg#icon-chat'></use>
+          </svg>
+          <span className='user-nav__notification'>13</span>
+        </div>
+        <div className='user-nav__user'>
+          <img
+            src='/images/user.jpg'
+            alt='User Photo'
+            className='user-nav__user__photo'
+          />
+          <span className='user-nav__name'>Jonas</span>
+        </div>
+      </nav>
     </header>
   );
 };
